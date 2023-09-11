e60e3dc2fb1d1919de656b0bd215ceb92cca89e9
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2023-09-11 05:01:54 +0200

    feat: parser - start

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,42 +1,45 @@
-import {ParseResult} from "./parseResult.ts";
 import {TokenType} from "./tokenType.ts";
 import {Token} from "./token.ts";
-import {COLON, COMMA, NEW_LINE} from "./tokens.ts";
 
 export class Parser {
-    public parse(data: string): ParseResult {
-        const result = new ParseResult()
-        const lines = data.split(NEW_LINE)
+    position = 0;
+    line = 1;
+    tokens: Token[] = [];
+    source = ''
+
+    public parse(source: string) {
         const tokens = []
         let buffer = []
-        for(let l = 0; l < lines.length;l++){
-            const line = lines[l]
-            let n = 0;
-            while(n < line.length){
-                const c = line.charAt(n)
-                switch(c){
-                    case COLON:
-                        tokens.push(new Token(TokenType.COLON))
-                        break;
-                    case COMMA:
-                        tokens.push(new Token(TokenType.COMMA))
-                        break;
-                    default:
-                       buffer.push(c)
-                }
-                n++
-            }
-            if(buffer.length > 0){
-                tokens.push(this.identifier(buffer))
+        while (this.position < source.length) {
+            const c = this.advance()
+            switch (c) {
+                case ' ':
+                case '\r':
+                case '\t':
+                    break;
+                case ':':
+                    tokens.push(new Token(TokenType.COLON))
+                    break;
+                case ',':
+                    tokens.push(new Token(TokenType.COMMA))
+                    break;
+                default:
+                    buffer.push(c)
             }
 
         }
-        return result
     }
 
-    private identifier(data: string[]): Token{
+    advance(): string {
+        return this.source.charAt(this.position++)
+    }
+    peek(): string {
+        return this.source.charAt(this.position)
+    }
+
+    private identifier(data: string[]): Token {
         const s = data.join()
 
-        return new Token(TokenType.IDENTIFIER,s)
+        return new Token(TokenType.IDENTIFIER, s)
     }
 }
--- src/parser/tokens.ts
+++
@@ -1,4 +0,0 @@
-export const SWP = '<->'
-export const COLON = ':'
-export const COMMA = ','
-export const NEW_LINE = '\n'
