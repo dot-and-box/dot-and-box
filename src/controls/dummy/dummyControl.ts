6e353ffb04dde3cc57a8358618bbaa4f240e9124
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2024-01-18 21:23:00 +0100

    feat: fix dummy control

--- src/controls/dummy/dummyControl.ts
+++ src/controls/dummy/dummyControl.ts
@@ -1,10 +1,13 @@
 import {Point} from "../../shared/point.ts";
 import {Control} from "../control.ts";
+import * as console from "console";
 
 export class DummyControl implements Control {
     id: string;
     position: Point;
     selected: boolean = false
+    visible: boolean = true
+
 
     public constructor() {
         this.id = 'dummy'
