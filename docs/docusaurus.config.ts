e8aecef482aa3f19fdc021477daa351560cbc63b
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2024-03-16 13:36:54 +0100

    feat: docs link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/tomaszkubacki/dots_and_boxes/tree/main/docs',
                     remarkPlugins: [
                         [
                             tabBlocks,
