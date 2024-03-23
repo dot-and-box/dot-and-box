a3252fceb321aa5dae80447d05a24b603c7ac037
Tomasz Kubacki <tomasz.kubacki@gmail.com>
2024-03-23 11:15:16 +0100

    fix: docs url after rename

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     url: 'https://dot-and-box.org',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/dot_and_box',
+    baseUrl: '/dot-and-box',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
@@ -22,7 +22,7 @@ const config: Config = {
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     scripts: [
-        'https://dot-and-box.github.io/dot_and_box/dot-and-box.js'
+        'https://dot-and-box.github.io/dot-and-box/dot-and-box.js'
     ],
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
